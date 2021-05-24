74599975ac89f5cc735c719594ff51921d12c8f5
Kalib Hicks <kalib.hicks@gmail.com>
2021-05-24 13:12:29 -0400

    troubleshooting

--- server.js
+++ server.js
@@ -5,11 +5,13 @@ const path = require('path');
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
 const router = require('./controllers/')
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
