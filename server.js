afae1fa84d5e37398c7207bae9ef448b62054381
Kalib Hicks <kalib.hicks@gmail.com>
2021-05-24 13:29:07 -0400

    troubleshooting

--- server.js
+++ server.js
@@ -1,17 +1,10 @@
 const express = require('express');
-//const routes = require('./routes');
+const routes = require('./routes');
 const sequelize = require('./config/connection');
 const path = require('path');
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
 const routes = require('./controllers/')
-
-const mysql = require('mysql2');
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: ''
-});
 
 const app = express();
 const PORT = process.env.PORT || 3006;
