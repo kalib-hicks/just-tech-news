217faf98a5898e7d03d4c6fb29ae14c2991b49e3
Kalib Hicks <kalib.hicks@gmail.com>
2021-05-24 13:17:45 -0400

    troubleshooting

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const routes = require('./routes');
+//const routes = require('./routes');
 const sequelize = require('./config/connection');
 const path = require('path');
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
-const router = require('./controllers/')
+const routes = require('./controllers/')
 
 
 const app = express();
